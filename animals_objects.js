c70e5359956825f9403657ef03d1a03547d4ccc4
anna <annabarellaalonso@gmail.com>
2023-02-22 20:23:11 +0100

    cleaned data and object

--- animals_objects.js
+++ animals_objects.js
@@ -2,9 +2,15 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
+const Animal = {
+    name: "-defaultname-",
+    desc: "-no desc-",
+    type: "-unknown-",
+    age: "-o-",
+}
 const allAnimals = [];
 
-function start( ) {
+function start() {
     console.log("ready");
 
     loadJSON();
@@ -13,17 +19,41 @@ function start( ) {
 
 function loadJSON() {
     fetch("animals.json")
-    .then( response => response.json() )
-    .then( jsonData => {
-        // when loaded, prepare objects
-        prepareObjects( jsonData );
-    });
+        .then(response => response.json())
+        .then(jsonData => {
+            // when loaded, prepare objects
+            prepareObjects(jsonData);
+        });
 }
 
-function prepareObjects( jsonData ) {
-    jsonData.forEach( jsonObject => {
+function prepareObjects(jsonData) {
+    jsonData.forEach(jsonObject => {
         // TODO: Create new object with cleaned data - and store that in the allAnimals array
-        
+        const animal = Object.create(Animal);
+        //Extract data form json Object
+        const fullname = jsonObject.fullname;
+
+        const firstSpace = fullname.indexOf(" ");
+        const secondSpace = fullname.indexOf(" ", firstSpace + 1);
+        const lastSpace = fullname.lastIndexOf(" ");
+
+        const name = fullname.substring(0, firstSpace);
+        const desc = fullname.substring(secondSpace + 1, lastSpace);
+        const type = fullname.substring(lastSpace + 1);
+
+        console.log(`name:${name} 
+        description:${desc} 
+        type:${type}`);
+
+        //put cleaned data into new object
+        animal.name = name;
+        animal.desc = desc;
+        animal.type = type;
+        animal.age = jsonObject.age;
+
+        //add the object to the global array
+        allAnimals.push(animal);
+
         // TODO: MISSING CODE HERE !!!
     });
 
@@ -35,10 +65,10 @@ function displayList() {
     document.querySelector("#list tbody").innerHTML = "";
 
     // build a new list
-    allAnimals.forEach( displayAnimal );
+    allAnimals.forEach(displayAnimal);
 }
 
-function displayAnimal( animal ) {
+function displayAnimal(animal) {
     // create clone
     const clone = document.querySelector("template#animal").content.cloneNode(true);
 
@@ -49,7 +79,7 @@ function displayAnimal( animal ) {
     clone.querySelector("[data-field=age]").textContent = animal.age;
 
     // append clone to list
-    document.querySelector("#list tbody").appendChild( clone );
+    document.querySelector("#list tbody").appendChild(clone);
 }
 
 
